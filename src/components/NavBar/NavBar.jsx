1c89fe0aa09956cbbfce7c501059f40978914620
LoretoGutierrezH <loretocgh@gmail.com>
2020-09-28 21:22:45 -0300

    ahora sí que sí xd

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,14 +7,14 @@ const NavBar = (props) => {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto" >
-          <Nav.Link className={style.linksColor} href="#features">Categorías</Nav.Link>
-          <Nav.Link href="#pricing">Recetas</Nav.Link>
-          <NavDropdown title="Selecciona tu local" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Independencia</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
+          <Nav.Link style={{color: "white"}} className={style.linksColor} href="#features">Categorías</Nav.Link>
+          <Nav.Link style={{color: "white"}} href="#pricing">Recetas</Nav.Link>
+          <NavDropdown style={{color: "white"}} title="Selecciona tu local" id="collasible-nav-dropdown">
+            <NavDropdown.Item style={{color: "white"}} href="#action/3.1">Independencia</NavDropdown.Item>
+            <NavDropdown.Item style={{color: "white"}} href="#action/3.2">
               Las Condes
             </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Maipú</NavDropdown.Item>
+            <NavDropdown.Item style={{color: "white"}} href="#action/3.3">Maipú</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Nav.Link href="#shopping-cart"><span className="iconify" data-icon="el:shopping-cart" data-inline="false"></span></Nav.Link>
