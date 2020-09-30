e77463a572b27ee012226e9f83dfac0f296dd33a
BarbaraMV <bmadariagav@gmail.com>
2020-09-29 21:18:57 -0300

    favicon, footer, recipes

--- src/components/Recipes/Recipe.jsx
+++ src/components/Recipes/Recipe.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import style from './Recipes.module.css';
 import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+
 
 const Recipe = (props) => {
   return (
-      <Card style={{ border: "none" }} className={style.cardContainer}>
-        <Link to="/recetas/saludables/lasaña-atún"><Card.Img
-          style={{ height: "22.2rem" }}
-          variant="top"
-          src={props.image}
-        /></Link>
+      <Card className={style.cardContainer}>
+        <Card.Img variant="top" src={props.image} />
         <Card.Body>
-          <Link className={style.recipeLink} to="/recetas/saludables/lasaña-atún"><Card.Title style={{fontFamily: "clarendonFamily", fontSize: "24px"}}>{props.name}</Card.Title></Link>
-          <Card.Text>{props.description}</Card.Text>
+          <Card.Title>{props.name}</Card.Title>
+          <Card.Text>
+            {props.description}
+          </Card.Text>
         </Card.Body>
       </Card>
+
+
   );
+
+
+
 }
 
 export default Recipe;
